1e15a099b98c5e9d8dc15197cea4bd2d37eb6256
RyotaKudo0816 <ryota.kudo.sys19@morijyobi.ac.jp>
2020-06-10 10:12:39 +0900

    orenonamarhakatouryousei

---
+++ src/Calc.java
@@ -0,0 +1,16 @@
+
+public class Calc {
+
+	public static void main(String[] args) {
+		int n1 = 10;
+		int n2 = 5;
+
+		int result1 = CalcLogic.addition(n1, n2);
+		int result2 = CalcLogic.subtraction(n1,n2);
+
+		System.out.println(result1);
+		System.out.println(result2);
+
+	}
+
+}
---
+++ src/CalcLogic.java
@@ -0,0 +1,12 @@
+
+public class CalcLogic {
+
+	public static int addition(int a, int b){
+		return a + b;
+	}
+
+	public static int subtraction(int a, int b){
+		return a - b;
+	}
+
+}
